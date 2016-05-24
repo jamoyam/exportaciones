333543046861ddc53f6acb13d5986beb0dcd6644
Jose Moya Moran <Jose Moya Moran@JMOYA-NT>
2016-05-23 20:51:33 -0400

    prueba update

---
+++ form/formcambioclave.php
@@ -0,0 +1,20 @@
+<?php
+$oUsuario=new Usuario();
+?>
+
+<form id="cambioclave" action="GET" >
+        <fieldset>
+        <legend>Personal information:</legend>
+        Clave Actual:<br>
+        <input type="text" name="claveactual" id="claveactual"><br>
+        Nueva Clave:<br>
+            <input type="text" name="clavenueva" id="clavenueva"><br>
+            <br>
+        Repetir Clave:<br>
+            <input type="text" name="repetirclave" id="repetirclave"><br>
+            <br>
+            <div id="mensaje"></div>
+            <input type="button" name="enviar" value ="Enviar" onclick="Cambiar()">
+            
+        </fieldset>
+</form>
